123674761fdee221ada430583509940a04ea47c9
Mattia Consiglio <37270576+mattia-consiglio@users.noreply.github.com>
2024-04-19 10:32:40 +0200

    initialize Spring Boot project with dependencies

---
+++ src/main/java/mattia/consiglio/eventmanagerapi/EventManagerApiApplication.java
@@ -0,0 +1,13 @@
+package mattia.consiglio.eventmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mattia/consiglio/eventmanagerapi/EventManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mattia.consiglio.eventmanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
